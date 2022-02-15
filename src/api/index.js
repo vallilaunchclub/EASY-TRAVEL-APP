a20922a9875eee150205c54ea5bd9a0116082e8b
vallilaunchclub <valli@thelaunchclub.in>
2022-02-15 10:56:11 +0530

    new project

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+export const getPlacesData = async (type,sw, ne) => {
+    try{
+        const { data:{ data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+         params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+            
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': '7514ac6c90mshbebd4daacbb7437p12960ajsndffe2800e9a4',
+          }
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React,{ useState } from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+import useStyles from './styles';
+const Header =({ setCoordinates }) => {
+    const classes = useStyles();
+     const [autocomplete,setAutoComplete] = useState(null);
+
+    const onLoad = (autoC) => setAutoComplete(autoC);
+
+    const onPlaceChanged = () => {
+         const lat = autocomplete.getPlace().geometry.location.lat();
+         const lng = autocomplete.getPlace().geometry.location.lng();
+
+      setCoordinates({ lat, lng });
+    }
+    return(
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" color="secondary" font-family="sans-serif"className={classes.title}>
+                    Easy Travel 
+                    </Typography>
+                    <Box display="flex">
+                    <Typography variant="h4"className={classes.title}>
+                    Explore the WORLD 
+                    </Typography>
+                      <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase placeholder="Search..." classes={{ root:classes.inputRoot,input:classes.inputInput}} />
+                        </div>
+                      </Autocomplete>
+                    </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
